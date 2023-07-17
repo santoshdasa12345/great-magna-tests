6d6f9ad3d9dd5ba346e24539062eebda38e517de
bobby-didcoding <bobby@didcoding.com>
2023-07-17 10:04:21 +0100

    Authenticating with vault_token_id

--- bootstrap.py
+++ bootstrap.py
@@ -6,15 +6,17 @@ class Vault:
     
     def __init__(self):
       self.ENVIRONMENT = sys.argv[1]
-      self.WORKSPACE = os.environ.get('WORKSPACE')
-      self.VAULT_API = "/".join(os.environ.get('VAULT_API').split("/")[:-1])
+      self.WORKSPACE = os.environ.get('WORKSPACE', '/home/jenkins/agent/workspace/great-load-test')
+      self.VAULT_API = os.environ.get('VAULT_API').replace('/v1', "")
       self.VAULT_PREFIX = os.environ.get('VAULT_PREFIX')
       self.VAULT_ROLE_ID = os.environ.get('VAULT_ROLE_ID')
-      self.VAULT_SECRET_ID = os.environ.get('VAULT_SERECT_ID')
+      self.VAULT_TOKEN_ID = os.environ.get('VAULT_TOKEN_ID')
+      self.VAULT_SECRET_ID = os.environ.get('VAULT_SECRET_ID')
       self.ENV_FILE = f"#{self.WORKSPACE}/great-magna-tests/env.json"
 
     def client(self):
 
+      print(self.VAULT_TOKEN_ID)
       # Create a client instance
       client = hvac.Client(self.VAULT_API)
       
@@ -26,7 +28,7 @@ class Vault:
       # Authenticate to Vault with role_id and secret
       client.auth.approle.login(
           role_id=self.VAULT_ROLE_ID,
-          secret_id=self.VAULT_SECRET_ID,
+          secret_id=self.VAULT_TOKEN_ID,
       )
 
       return client
